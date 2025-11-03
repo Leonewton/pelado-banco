d04dc2f8d471c1e708f1f94a18df8f4d35121433
marcelo0404 <marcelo.vieira13@outlook.com.br>
2025-11-03 14:46:31 -0300

    Criando a Classe Banco e testando no Main

--- Main.java
+++ Main.java
@@ -1,13 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("João Silva", "123.456.789-00", "Rua das Flores, 123");
-        System.out.println(cliente.toString()); 
-        
-        Conta conta = new ContaCorrente("0001", cliente, 1000.0);
-        System.out.println(conta.toString());
-            
-        Conta contaPoupanca = new ContaPoupanca("0002", cliente, 500.0, 2.0);
-        System.out.println(contaPoupanca.toString());
+        System.out.println("Bem-vindo ao Pelado Banco!");
+        Banco PeladoBanco = new Banco();
+        System.out.println();
+
+        System.out.println("Cadastrando clientes");
+        PeladoBanco.cadastrarCliente("João Silva", "12345678900", "Rua A, 123");
+        System.out.println();
+        PeladoBanco.cadastrarCliente("Maria Oliveira", "98765432100", "Avenida B, 456");
+        System.out.println();
+
+        System.out.println("Teste CPF duplicado:");
+        PeladoBanco.cadastrarCliente("João Silva", "12345678900", "Rua A, 123");
+        System.out.println();
+
+        PeladoBanco.listarClientes();
+        System.out.println();
+
+        System.out.println("Criando contas para os clientes:");
+        PeladoBanco.criarConta("12345678900", "corrente", 1000);
+        System.out.println();
+        PeladoBanco.criarConta("98765432100", "poupanca", 500);
+        System.out.println();
+
+        System.out.println("Teste CPF não cadastrado:");
+        PeladoBanco.criarConta("11122233344", "corrente", 300);
+        System.out.println();
+
+        System.out.println("Teste tipo de conta inválido:");
+        PeladoBanco.criarConta("12345678900", "investimento", 1000);
+        System.out.println();
+
+        PeladoBanco.listarContas();
+        System.out.println();
+
+
+
     };
 
 
